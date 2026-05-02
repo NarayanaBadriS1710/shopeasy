2d1a40ede036fb6a816d7ed5681e29e483e02511
Narayana Badri <narayanabadri17@gmail.com>
2026-05-02 18:54:00 +0530

    fix csrf error

--- prj_shopeasy/settings.py
+++ prj_shopeasy/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-yy2*!m($hr+$s=6hx6j2nzvdgkem#4cw$v$84imm10*f7ga3x4')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://shopeasy-production-e963.up.railway.app']
 
 # Application definition
 INSTALLED_APPS = [
