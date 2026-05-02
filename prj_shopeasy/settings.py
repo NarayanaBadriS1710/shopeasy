73b95079993a4a9b81b3661d7fb27634e536f4ca
Narayana Badri <narayanabadri17@gmail.com>
2026-05-02 18:46:50 +0530

    fix media files

--- prj_shopeasy/settings.py
+++ prj_shopeasy/settings.py
@@ -88,3 +88,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
