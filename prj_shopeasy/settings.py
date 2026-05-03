94fd1cb2f6eaf485e9153f4294ede1bd0fb70488
Narayana Badri <narayanabadri17@gmail.com>
2026-05-03 19:00:17 +0530

    fix local database config

--- prj_shopeasy/settings.py
+++ prj_shopeasy/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'prj_shopeasy.wsgi.application'
 # Database - PostgreSQL
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
