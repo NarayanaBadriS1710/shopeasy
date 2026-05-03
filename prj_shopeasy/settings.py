6a17c91f7f34a65df62823d05283471f6f824b54
Narayana Badri <narayanabadri17@gmail.com>
2026-05-03 17:49:54 +0530

    clean settings with secure cloudinary

--- prj_shopeasy/settings.py
+++ prj_shopeasy/settings.py
@@ -103,13 +103,15 @@ CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 cloudinary.config(
     cloud_name = os.environ.get('CLOUD_NAME'),
     api_key = os.environ.get('CLOUDINARY_API_KEY'),
-    api_secret = os.environ.get('CLOUDINARY_API_SECRET')
+    api_secret = os.environ.get('CLOUDINARY_API_SECRET'),
+    secure = True
 )
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE': True,
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
